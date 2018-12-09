b9688eef9edc794db596b8319c61e96062b43260
Maciej Garbacz <2203950g@student.gla.ac.uk>
2018-12-09 19:10:07 +0000

    Minor changes to Second Example

--- demo/src/main/java/com/example/demo/DemoController.java
+++ demo/src/main/java/com/example/demo/DemoController.java
@@ -1,14 +1,21 @@
 package com.example.demo;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+
 
 @RestController
 public class DemoController {
+
+    public Logger LOGGER = LoggerFactory.getLogger(DemoController.class);
 
     @Autowired
     EmployeeJDBCRepository repository;
@@ -16,5 +23,10 @@ public class DemoController {
     @RequestMapping("/employees")
     public List<Employee> employees(){
         return repository.findAll();
+    }
+
+    @Scheduled(fixedRate = 5000)
+    public void logTime(){
+        LOGGER.info(LocalDateTime.now().toString());
     }
 }
